06be7be17cd72e240bf448ac19e5112eef193bf6
falcoxcorp <falcoxcorp@gmail.com>
2025-09-27 00:48:04 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
+import replace from '@rollup/plugin-replace'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,18 +16,23 @@ export default defineConfig({
     }
   },
   plugins: [
-    react({
-      jsxRuntime: 'automatic'
-    })
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+      preventAssignment: true
+    }),
+    react()
   ],
   optimizeDeps: {
     include: ['react', 'react-dom'],
     exclude: ['ethers'],
-    force: false
+    force: false,
+    esbuildOptions: {
+      target: 'es2015'
+    }
   },
   define: {
     global: 'globalThis',
-    'process.env': {}
+    'process.env.NODE_ENV': '"development"'
   },
   server: {
     port: 3000,
